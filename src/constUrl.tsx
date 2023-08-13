b980422b7e71b0fe8b5e4fd5393a89af484349e2
Arnohollowknight <maximzalupenko@yandex.ru>
2023-08-13 15:08:24 +0400

    add linkProj

--- src/constUrl.tsx
+++ src/constUrl.tsx
@@ -2,7 +2,7 @@ type Link = string | undefined;
 
 const repoLink: Link = "https://github.com/Arnohollowknight/Movie-project";
 
-const mainPageLink: Link = "https://github.com/Arnohollowknight/Movie-project"
+const mainPageLink: Link = "https://arnohollowknight.github.io/Movie-project/"
 
 const API_KEY: Link = process.env.REACT_APP_API_KEY;
 
